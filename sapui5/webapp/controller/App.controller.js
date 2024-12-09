e101867e76059476c99ce4a3e60739d861b4971c
ridha bouyahi <ridha.bouyahyi@outlook.com>
2024-12-09 09:48:08 +0000

    Step 1 (fixing MockServer configuration)

--- sapui5/webapp/controller/App.controller.js
+++ sapui5/webapp/controller/App.controller.js
@@ -1,10 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
+], (Controller,JSONModel,MessageBox,MessageToast) => {
     "use strict";
 
     return Controller.extend("odatav4.sapui5.controller.App", {
         onInit() {
+            var oJSONData = {
+                busy : false 
+            };
+            var oModel = new JSONModel(oJSONData) ;
+            this.getView().setModel(oModel,"appView")
         }
     });
 });
